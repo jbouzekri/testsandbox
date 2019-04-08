85b1453368fdb99dde7cb33bba222477161f3b7c
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2019-04-08 22:08:02 +0200

    test

--- src/test.php
+++ src/test.php
@@ -1,70 +1,9 @@
 <?php
 
-
-if (true)
-{
-echo 'not psr at all';
+if (true) {
+    echo 'not psr at all';
 }
 
 function test($name) {
     echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
 }
-
-function test2($name) {
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-    echo "totally useless cpd detector function";
-}
-?>
